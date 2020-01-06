848d7999d1d065541d1519c9c68f12b5a8b06e43
Rafael Beraldo <rafael.pimpa@gmail.com>
2020-01-06 13:32:12 -0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,17 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
+# navigate to build dir
 cd dist
 
+# deploy on GH pages
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:FurySim/furysim.github.io.git master
 
+# back to root
 cd -
+
+# push tags (releases)
+git push --tags
